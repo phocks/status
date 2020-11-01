9c173de9fa9b254cf761f20c3316a6ab94145e3a
Joshua Byrd <phocks@gmail.com>
2020-11-01 20:46:12 +1000

    Some testing

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
 // Deno cli argument parser
 import { parse } from "https://deno.land/std@0.76.0/flags/mod.ts";
+import { sleep } from "https://deno.land/x/sleep@v1.2.0/mod.ts";
 
 const args = parse(Deno.args);
+console.dir(args);
+
+// Default response
+if (args._.length === 0) {
+  console.log("Enter a command");
+}
 
 // Simple hello world
 if (args._[0] === "hello") {
@@ -14,4 +21,12 @@ if (args._[0] === "count") {
   const json = await response.json();
 
   console.dir(json);
+
+  // for (let i = 0; i < 100; i++) {
+  //   fetch("https://api.rise.gq/")
+  //     .then((res) => res.json())
+  //     .then((jsonData) => console.log(jsonData));
+
+  //   await sleep(0.1);
+  // }
 }
